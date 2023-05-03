e9b21bef3626fe019e603834c5e62bab3ce1ddd0
KapilDagur <kapildagur1306@gmail.com>
2023-05-03 22:48:05 +0530

    Requirement Text Added Here with File Download code at client machine

--- Stream_Downloader/Youtube_Downloader_app/views.py
+++ Stream_Downloader/Youtube_Downloader_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from pathlib import Path
+from django.http import FileResponse
+from pytube import YouTube
 
 # Create your views here.
+def youtubeQueryFetcher(request):
+    file_to_download = YouTube("https://www.youtube.com/watch?v=txXXRMfp12w")
+    response = FileResponse(file_to_download,content_type = "application/force-download")
+    response['Content-Disposition'] = 'inline ; filename = "file.md"'
+    return response
